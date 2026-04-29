8e4cc77e0e922858da81f3cce2031560204892f1
Hubert Kecler <hubert.kecler@spindox.it>
2026-04-29 14:54:56 +0200

    Added filters

--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Task
 
 def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, "tasks.html", {"tasks": tasks})
+    filter_param = request.GET.get('filter', 'all')
+
+    if filter_param == 'completed':
+        tasks = Task.objects.filter(completed=True)
+    elif filter_param == 'incomplete':
+        tasks = Task.objects.filter(completed=False)
+    else:
+        tasks = Task.objects.all()
+
+    return render(request, 'tasks.html', {'tasks': tasks, 'filter_param': filter_param})
 
 def task_detail(request, id):
     task = Task.objects.get(pk= id)
